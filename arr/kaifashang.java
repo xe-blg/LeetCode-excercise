aa480b076e845ecf687c7ae42efec9fd5e361a9e
xe-blg <18101332902@163.com>
2026-04-22 10:26:24 +0800

    add_kaifashang

---
+++ arr/kaifashang.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class kaifashang {
+     public static void main(String[] args)
+     {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int sum = 0;
+        int[][] vec = new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;i<m;j++)
+            {
+                vec[i][j] = scanner.nextInt();
+                sum = sum+vec[i][j];
+            }
+        }
+
+        int result = Integer.MAX_VALUE;
+        int count = 0; // 统计遍历过的行
+
+        // 行切分
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                count += vec[i][j];
+                // 遍历到行末尾时候开始统计
+                if (j == m - 1) {
+                    result = Math.min(result, Math.abs(sum - 2 * count));
+                }
+            }
+        }
+
+        count = 0;
+        // 列切分
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                count += vec[i][j];
+                // 遍历到列末尾时候开始统计
+                if (i == n - 1) {
+                    result = Math.min(result, Math.abs(sum - 2 * count));
+                }
+            }
+        }
+     }
+}
